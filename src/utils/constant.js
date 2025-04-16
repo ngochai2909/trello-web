8b0d039e0afacf604401799e9a7b5c853bc251a8
nguyenngochai2909 <nguyenngochai290902@gmail.com>
2025-04-16 08:34:33 +0700

    fix cors

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,8 +1,9 @@
 let apiRoot = ''
 if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
-} else if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'http://trello-web-lake-eight.vercel.app/'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'http://trello-web-lake-eight.vercel.app'
 }
 
 export const API_ROOT = apiRoot
