5fcf88a259cd2700a30ca0538778d0bb000a5471
nguyenngochai2909 <nguyenngochai290902@gmail.com>
2025-04-16 09:47:16 +0700

    fix build production

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,7 +3,7 @@ if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
 }
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'http://trello-web-lake-eight.vercel.app'
+  apiRoot = 'https://trello-api-cutq.onrender.com'
 }
 
 export const API_ROOT = apiRoot
