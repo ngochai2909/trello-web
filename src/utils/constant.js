cc96c678fee132422e153e617d8137cbcadb0822
nguyenngochai2909 <nguyenngochai290902@gmail.com>
2025-04-16 08:19:46 +0700

    fix build mode dev

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,8 @@
-export const API_ROOT = 'http://localhost:8017'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+} else if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'http://trello-web-lake-eight.vercel.app/'
+}
+
+export const API_ROOT = apiRoot
